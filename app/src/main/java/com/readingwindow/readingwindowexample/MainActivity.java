ef50dac2833cb417267723d7d4f2f1f9f23c7ee7
ishang <ishan.g@squareinfosoft.com>
2018-04-19 18:32:04 +0530

    Solve bugs

--- app/src/main/java/com/readingwindow/readingwindowexample/MainActivity.java
+++ app/src/main/java/com/readingwindow/readingwindowexample/MainActivity.java
@@ -1,7 +1,7 @@
 package com.readingwindow.readingwindowexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -9,7 +9,7 @@ import android.webkit.WebView;
 import com.readingwindow.ReadingWindow;
 
 public class MainActivity extends AppCompatActivity {
-
+    ReadingWindow readingWindow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +18,14 @@ public class MainActivity extends AppCompatActivity {
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         myWebView.loadUrl("https://blog.trello.com/automation-magic-trello-boards");
-
+         readingWindow=findViewById(R.id.readingWindow);
         findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ReadingWindow.isVisible()) {
-                    ReadingWindow.hide();
+                if (readingWindow.getVisibility()==View.VISIBLE) {
+                    readingWindow.setVisibility(View.GONE);
                 } else {
-                    ReadingWindow.show(MainActivity.this);
+                    readingWindow.setVisibility(View.VISIBLE);
                 }
             }
         });
