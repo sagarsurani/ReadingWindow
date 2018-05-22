c2882fdba892e5d632e6f7f050615d1b286a7e00
ishang <ishan.g@squareinfosoft.com>
2018-05-22 18:09:50 +0530

    create a new method like setbgcolor,setalph,setoverlay

--- app/src/main/java/com/readingwindow/readingwindowexample/MainActivity.java
+++ app/src/main/java/com/readingwindow/readingwindowexample/MainActivity.java
@@ -10,6 +10,10 @@ import com.readingwindow.ReadingWindow;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    int[] color=new int[]{R.color.colorAccent,R.color.colorPrimary,R.color.colorPrimaryDark,R.color.red};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +30,29 @@ public class MainActivity extends AppCompatActivity {
                     ReadingWindow.hide();
                 } else {
                     ReadingWindow.show(MainActivity.this);
+                   ReadingWindow.setDefaultSize(100,200);
+                   ReadingWindow.setbackground(getResources().getColor(color[1]));
+
+
+
+                   ReadingWindow.setReadingAreaColor(getResources().getColor(color[0]));
+                    ReadingWindow.setReadingAreaAlpha(0.5f);
+                   ReadingWindow.setBackgroundAlpha(0.4f);
+
+
+
                 }
             }
         });
+
+
+
+
+
+
+
     }
+
 
 
 
